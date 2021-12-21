83dcf85fe468b9e54a580969391b2e59d2735f25
NelsonBeard <ilnur111111yandex.ru>
2021-12-21 21:25:27 +0500

    Сделал получение данных с сервера. Пока не работает

---
+++ app/src/main/java/com/geekbrains/weather/model/WeatherDTO.kt
@@ -0,0 +1,13 @@
+package com.geekbrains.weather.model
+
+data class WeatherDTO(
+    val now: Long?,
+    val fact: FactDTO?
+)
+
+data class FactDTO(
+    val temp: Int?,
+    val feels_like: Int?,
+    val condition: String?,
+    val humidity: Int?
+)
