6e1264bad200a738255b3c3f877b1a76a0fd1feb
NelsonBeard <ilnur111111yandex.ru>
2022-01-27 14:27:04 +0500

    добавил платную и бесплатную версии приложения

---
+++ app/src/free/java/com/geekbrains/weather/view/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.geekbrains.weather.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.geekbrains.weather.R
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
+
+
+
+
+
+
+
